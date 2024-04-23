dd326389135aef24db18937fa5452a42fe86328a
Ethan <99791261+EthanTK03@users.noreply.github.com>
2024-04-23 16:06:51 -0700

    Update and rename loginApp.py to registerApp.py

--- HTML files/registerApp.py
+++ HTML files/registerApp.py
@@ -12,8 +12,8 @@ db = mysql.connector.connect(
 )
 
 @app.route('/')
-def login_page():
-    return render_template('login.html')
+def register_page():
+    return render_template('register.html')
 
 @app.route('/register', methods=['POST'])
 def register():
