3bd22ad1944f856aff1ed7f4cc6845909f776a1a
shickmanIV <samuel.hickman@wsu.edu>
2024-04-25 16:26:55 -0700

    main.py now dynamically loads all routes from the routes directory

--- ShopTCG/main.py
+++ ShopTCG/main.py
@@ -1,20 +1,25 @@
 import os
 from importlib import import_module
 from flask import Flask, redirect, url_for
+import routes.register as reg #.testroute2 as t2
+import routes.login as log
 
 app = Flask(__name__)
 
 # Dynamically import and register all routes from the routes package
+# Must run 'python main.py' INSIDE the ShopTCG directory
 for root, dirs, files in os.walk("routes"):
     for file in files:
         if file.endswith(".py") and file != "__init__.py":
             module_name = f"routes.{file[:-3]}"  # Convert file path to module path
             module = import_module(module_name)
-            app.register_blueprint(getattr(module, 'bp'))
+            bp = getattr(module, 'bp', None)
+            if bp:
+                app.register_blueprint(bp)
 
 @app.route('/')
 def index():
-    return redirect(url_for('testroutes_2.route2_method'))
+    return redirect(url_for('register_logic.register_page'))
 
 if __name__ == "__main__":
     app.run(debug=True)
