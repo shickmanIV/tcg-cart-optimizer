4838f1b31997d3b3269f39d169d46581109068aa
Ethan <99791261+EthanTK03@users.noreply.github.com>
2024-04-30 22:09:20 -0700

    Update registerApp.py Added ability to select user type
    Buyer or Seller, this information is now stored in the database

--- ShopTCG/routes/registerApp.py
+++ ShopTCG/routes/registerApp.py
@@ -5,14 +5,18 @@ def registerUser(form_data):
     password = form_data['password']
     first_name = form_data['first_name']
     last_name = form_data['last_name']
+    account_type = form_data['accountType']\
+
+    buyer = 1 if account_type == 'Buyer' else 0
+    seller = 1 if account_type == 'Seller' else 0
 
     print("registerUser() called")
     print(form_data)
 
     # Insert user data into the database
     cursor = db.cursor()
-    sql = "INSERT INTO users (Username, Password, FirstName, LastName) VALUES (%s, %s, %s, %s)"
-    val = (username, password, first_name, last_name)
+    sql = "INSERT INTO users (Username, Password, FirstName, LastName, Buyer, Seller) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (username, password, first_name, last_name, buyer, seller)
     cursor.execute(sql, val)
     db.commit()
 
