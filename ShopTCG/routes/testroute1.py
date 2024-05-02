fc17afcf1f85e1ed960deb18725754dc98279a8c
shickmanIV <samuel.hickman@wsu.edu>
2024-05-02 11:09:12 -0700

    removed flask test routes

--- ShopTCG/routes/testroute1.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('testroutes_1', __name__)
-
-@bp.route('/route1')
-def route1_method():
-    error = None
-    return render_template('testpage1.html', error=error)
--- ShopTCG/routes/testroute2.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('testroutes_2', __name__)
-
-@bp.route('/route2')
-def route2_method():
-    error = None
-    return render_template('testpage2.html', error=error)
